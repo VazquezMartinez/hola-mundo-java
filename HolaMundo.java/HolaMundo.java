bbe4e62fbb2dcf641b9984bc77119b50c5e9cad3
Ivan Vazquez <ivan.vazquez19@uabc.edu.mx>
2026-08-26 09:57:52 -0700

    Modificacion codigo

--- HolaMundo.java/HolaMundo.java
+++ HolaMundo.java/HolaMundo.java
@@ -8,6 +8,6 @@
 public class HolaMundo
 {
     public static void main(String[ ] args) {
-        System.out.println("Hola mundo desde Java!");
+        System.out.println("Adios mundo desde Java!");
     }
 }
